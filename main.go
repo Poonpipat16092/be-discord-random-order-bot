42ca6b172453404ccfb469799d30b8230e11fdfb
Poonpipat(Arm) <poonpipat.k@lmwn.com>
2022-09-19 21:47:16 +0700

    add log

--- main.go
+++ main.go
@@ -7,32 +7,36 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/PurpleSec/logx"
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
 	Token string
 )
+var con = logx.Console(logx.Info)
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
+	con.Info("TOKEN:", Token)
 }
 func main() {
 	//Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
+	session, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	session.AddHandler(messageCreate)
+	con.Info(session.Token)
 
 	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	session.Identify.Intents = discordgo.IntentsGuildMessages
 
 	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
+	err = session.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
@@ -45,25 +49,27 @@ func main() {
 	<-sc
 
 	// Cleanly close down the Discord session.
-	dg.Close()
+	session.Close()
 }
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
+	con.Info("ID: %s, Content: %s", m.Author.Username, m.Content)
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
+	// If the messasge is "ping" reply with "Pong!"
 	if m.Content == "ping" {
+		con.Info("Ping in")
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
 
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
+		con.Info("pong in")
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 }
